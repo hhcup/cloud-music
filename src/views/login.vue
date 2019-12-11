2e6d1f9755b6d9a5aa96e7f8e3301b2421b5923c
hhcup <h714690319@163.com>
2019-12-11 20:35:02 +0800

    登录页样式修改

--- src/views/login.vue
+++ src/views/login.vue
@@ -3,6 +3,19 @@
         <div class="logo">
             <div class="logo-img"></div>
         </div>
+        <div class="login-box">
+            <div class="login-btn">手机号登录</div>
+            <div class="three-login-box">
+                <a href="#" class="three-login-item"></a>
+                <a href="#" class="three-login-item"></a>
+                <a href="#" class="three-login-item"></a>
+                <a href="#" class="three-login-item"></a>
+            </div>
+            <div class="login-agreement">
+                <input id="login" type="checkbox" name="agree"><span class="agree">同意</span>
+                <p>《用户协议》&nbsp;《隐私政策》&nbsp;《儿童隐私政策》</p>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -15,12 +28,14 @@
     }
 </script>
 <style lang='scss' scoped>
+@import "@/assets/css/common.scss";
+@import "@/assets/css/reset.scss";
      ::v-deep.login{
         position: relative;
         height: 100%;
         width: 100%;
-        background-color: #e60012;
-        /deep/ .logo{
+        background-color: $baseColor;
+        .logo{
             position: absolute;
             left: 50%;
             top: 180px;
@@ -30,6 +45,77 @@
             border-radius: 50%;
             background: red url("../assets/icons/logo.png") no-repeat center;
             background-size: 70%;
+        }
+        .login-box{
+            position: absolute;
+            bottom: 20px;
+            left: 50%;
+            width: 80%;
+            transform: translateX(-50%);
+            .login-btn{
+                width: 100%;
+                height: 40px;
+                margin-bottom: 20px;
+                text-align: center;
+                line-height: 40px;
+                font-size: 16px;
+                color:$baseColor;
+                border-radius: 20px;
+                letter-spacing: 2px;
+                background-color: white;
+            }
+            .three-login-box{
+                display: flex;
+                justify-content: space-around;
+                width: 100%;
+                .three-login-item{
+                    display: block;
+                    width: 30px;
+                    height: 30px;
+                    border: 1px solid white;
+                    border-radius: 50%;
+                    box-sizing: border-box;
+                    background-size: 22px;
+                    background-repeat: no-repeat;
+                    background-position: center;
+                    &:nth-child(1){
+                        background-image: url("../assets/icons/weixin_logo.png");
+                        background-position: 50% 60%;
+                    }
+                    &:nth-child(2){
+                        background-image: url("../assets/icons/qq_logo.png");
+                    }
+                    &:nth-child(3){
+                        background-image: url("../assets/icons/weibo_logo.png");
+                    }
+                    &:nth-child(4){
+                        background-image: url("../assets/icons/wangyi_logo.png");
+                    }
+                }
+            }
+            .login-agreement{
+                display: flex;
+                justify-content: center;
+                width: 100%;
+                margin-top: 20px;
+                #login{
+                    width: 15px;
+                    height: 15px;
+                    border-radius: 2px!important;
+                    border: 1px solid #999;
+                    margin-right: 5px;
+                    background-color: unset!important;
+                }
+                span{
+                    color: #999;
+                    line-height: 17px;
+                }
+                p{
+                    color: white;
+                    line-height: 17px;
+                    font-weight: lighter;
+                }
+            }
         }
     }
 </style>
